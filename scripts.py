481be51a8a0df7ca9d6c22c69cc2ba7b3b867c3f
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2020-11-14 23:50:19 +0900

    リストの作成をCSV読み込みに変更

--- scripts.py
+++ scripts.py
@@ -5,14 +5,12 @@ import itertools
 ### これをベースに課題の内容を追記してください
 
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+# source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 # csvからの取得
-f = open("キャラクター.csv", "r")
-reader = csv.reader(f)
-data = list(itertools.chain.from_iterable([ e for e in reader ]))
-f.close()
-source.extend(data)
+with open('character.csv', 'r', newline = '') as f:
+    reader = csv.reader(f)
+    source = list(itertools.chain.from_iterable([ e for e in reader ]))
 
 ### 検索ツール
 def search():
@@ -23,7 +21,13 @@ def search():
         print("{0}が見つかりました".format(word))
     else:
         print("{0}は見つかりませんでした、登場人物リストに追加します。".format(word))
+        # リストへの追加
         source.append(word)
+        # CSVファイルへの追加
+        with open('character.csv', 'a', newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerow([word])
+
 
         # 見つからなかった場合、追加出来たか確認のためもう一度同じ処理を通す
         word =input("もう一度鬼滅の登場人物の名前を入力してください >>> ")
